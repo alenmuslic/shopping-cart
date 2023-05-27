5cf77a19e35f37318f5a0287ffbb01fd4c897605
alenmuslic <muslicalen15@gmail.com>
2023-05-27 18:46:32 +0200

    contextProvider

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,10 +1,51 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import "./Products.css";
+import Pagination from "@mui/material/Pagination";
 import ProductCard from "../../components/Card/ProductCard";
+import { toast } from "react-hot-toast";
+import { AppContext } from "../../context/AppContext";
 
 export default function Products() {
+  const { products, addToCart, cart } = useContext(AppContext);
+  const [page, setPage] = useState(1);
+  const handleChange = (event, value) => {
+    setPage(value);
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  };
+  const productsPerPage = 15;
+  const numOfPages = Math.ceil(products.length / productsPerPage);
+
+  console.log(products);
+  console.log(cart);
   return (
     <>
-      <ProductCard />
+      <div className="cards">
+        {products
+          .map((product) => (
+            <ProductCard
+              key={product.id}
+              id={product.id}
+              productName={product.title}
+              productPrice={product.price}
+              productImage={product.imageURL}
+              addToCart={() => {
+                addToCart(product.id);
+              }}
+              deleteFromCart={() => {
+                toast.success("Successfully deleted from cart!");
+              }}
+            />
+          ))
+          .slice((page - 1) * productsPerPage, page * productsPerPage)}
+      </div>
+      <div className="pagination">
+        <Pagination
+          size="large"
+          count={numOfPages}
+          page={page}
+          onChange={handleChange}
+        />
+      </div>
     </>
   );
 }
